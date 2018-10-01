2cd2a8ccd7df1cbe6e3d0ee15effd5a5fe626503
Ineigo <ineigokira@gmail.com>
2018-10-01 11:49:53 +0300

    Сделал первичную настройку webpack

---
+++ client/Application.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>{{ message }}</h1>
+</template>
+
+<script>
+import style from './main.style.scss';
+
+export default {
+    data() {
+        return {
+            message: 'Hello !!!',
+        };
+    },
+};
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './client/index.js',
+    },
+
+    output: {
+        filename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    extractCSS: true,
+                },
+            },
+            {
+                include: [path.resolve(__dirname, 'client')],
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: [require('@babel/plugin-proposal-object-rest-spread')],
+                },
+                test: /\.js$/,
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, { loader: 'css-loader' }, { loader: 'sass-loader' }],
+            },
+        ],
+    },
+
+    mode: 'development',
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new UglifyJSPlugin(),
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash].css',
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Словарикин',
+            template: 'client/index.html',
+            filename: 'index.html',
+            appMountId: 'app',
+        }),
+    ],
+
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.js',
+        },
+    },
+
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendors: {
+                    priority: -10,
+                    test: /[\\/]node_modules[\\/]/,
+                },
+            },
+
+            chunks: 'async',
+            minChunks: 1,
+            minSize: 30000,
+            name: true,
+        },
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+};
