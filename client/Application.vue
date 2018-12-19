87798d76a697e82aac6bbfc920ee38c59f356e4e
Ineigo <ineigokira@gmail.com>
2018-12-19 13:44:52 +0300

    Разобрался с ошибкой в vscode про импорт json

--- client/Application.vue
+++ client/Application.vue
@@ -1,13 +1,14 @@
 <template>
     <div>
         <top-bar/>
-        <card-list/>
+        <!-- <card-list/> -->
     </div>
 </template>
 
 <script lang="ts">
 import TopBar from './components/top-bar/TopBar.vue';
 import CardList from './components/card-list/CardList.vue';
+import * as list from './data/jquery.data.json';
 
 export default {
     name: 'Application',
